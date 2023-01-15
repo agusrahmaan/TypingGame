f349c657b4bc3ffeb3c3a22d850bb38d009890cf
zyoler <zyoler@gmail.com>
2023-01-16 00:06:42 +0700

    Add project files.

---
+++ TypingGame/NewFolder/Stats.cs
@@ -0,0 +1,20 @@
+﻿namespace TypingGame.NewFolder
+{
+    public class Stats
+    {
+        public int Correct;
+        public int Missed;
+        public int Total;
+        public int Accuracy;
+
+        public void CountTotal()
+        {
+            Total = Correct + Missed;
+        }
+
+        public void CountAccuracy()
+        {
+            Accuracy = Correct * 100 / (Correct+Missed);
+        }
+    }
+}
